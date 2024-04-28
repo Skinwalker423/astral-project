9c726e82837d49834cabbb2e9038d6691c99043f
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2024-04-27 23:39:59 -0700

    404 layout

---
+++ constants.ts
@@ -0,0 +1,28 @@
+/**
+ * Name of the website title
+ * @type {(string)}
+ */
+
+export const SITE_TITLE = "TechPeople Blog";
+
+/**
+ * Description and goal of the site
+ * @type {(string)}
+ */
+
+export const SITE_DESCRIPTION =
+  "Articles, Stories & Tutorials for Tech People";
+
+/**
+ * Maximun Number of articles that will be displayed on the homepage
+ * @type {(number)}
+ */
+
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+
+/**
+ * Maximun Number of articles that will be displayed per page for pagination
+ * @type {(number)}
+ */
+
+export const ARTICLES_PER_PAGE = 6;
